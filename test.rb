922c2a4da6b82377f9bef7b02b60e1ebf550ac3a
unixsystems <unixsystems@gmail.com>
2018-02-19 12:07:01 +0000

    Fixed test.rb to run successfully

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ def app
 end
 def test_my_default
   get '/'
-  assert_equal 'BHello, world!', last_response.body
+  assert_equal 'Hello, world!', last_response.body
 end
 def test_with_params
   get '/Frank'
